9705071e473dcaa58b5f90a01544cb6b7cf1ffe9
guipaterlini <guilhermepaterlini@gmail.com>
2023-01-29 17:04:52 -0400

    Feat: ajustado a experiencia no mobile com o botão hamburguer de menu

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,10 @@
 const btnMobile = document.getElementById("btn-mobile");
 
-function toggleMenu() {
+function toggleMenu(event) {
+    // o if com event.preventdefault está aqui para que no mobile o menu nao seja aberto e em seguida fechado, 
+    // por o eventlistener está com click e touchstart
+  if (event.type == "touchstart") event.preventDefault();
+
   const nav = document.getElementById("nav");
   const span = document.getElementById("btn-mobile");
   nav.classList.toggle("active");
@@ -8,3 +12,6 @@ function toggleMenu() {
 }
 
 btnMobile.addEventListener("click", toggleMenu);
+// É colocando o touchstart porque no mobile teria mais ou menos 300ms antes de começar o click, 
+// o que ficaria estranho.
+btnMobile.addEventListener("touchstart", toggleMenu);
