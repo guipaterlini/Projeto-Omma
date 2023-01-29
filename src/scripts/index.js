96df6cafc7a0c4801e68bc55524f746e3d724861
guipaterlini <guilhermepaterlini@gmail.com>
2023-01-29 16:58:20 -0400

    Feat: adicionado animação para menu hamburguer no mobile

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,7 +2,9 @@ const btnMobile = document.getElementById("btn-mobile");
 
 function toggleMenu() {
   const nav = document.getElementById("nav");
+  const span = document.getElementById("btn-mobile");
   nav.classList.toggle("active");
+  span.classList.toggle("active");
 }
 
 btnMobile.addEventListener("click", toggleMenu);
