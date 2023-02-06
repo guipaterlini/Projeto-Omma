29f0e24718946983b54b63076106ad659991b3f2
guipaterlini <guilhermepaterlini@gmail.com>
2023-02-06 19:34:49 -0400

    Feat: adicionado função para criar receitas

--- src/utils/criarReceita.js
+++ src/utils/criarReceita.js
@@ -1,2 +1,25 @@
-function criarReceit() {}
- 
+import receitasTextos from "../common/receitasTextos.mjs";
+import crypto from "crypto";
+
+function criarReceita({
+  titulo,
+  dificuldade,
+  ingredientes,
+  instrucoes,
+  linkVideo,
+  vegana,
+}) {
+  const id = crypto.randomUUID();
+
+  receitas.push({
+    id,
+    titulo,
+    dificuldade,
+    ingredientes,
+    instrucoes,
+    linkVideo,
+    vegana,
+  });
+}
+
+export { criarReceita };
